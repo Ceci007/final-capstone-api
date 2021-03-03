0c3c7b08d31be20fdff6fcfc88bd298822004b04
Ceci007 <cecibenitezca@gmail.com>
2021-03-03 09:42:33 -0300

    Added route for illnesses

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_page#home'
   post '/login', to: 'sessions#create'
+  post '/createill', to: 'illnesses#create'
   delete '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
   resources :users, only: %i[create show index] do
