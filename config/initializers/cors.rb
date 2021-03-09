7ff5b5c10550981e4c2f3dc459431e8d03bb4c11
Ceci007 <cecibenitezca@gmail.com>
2021-03-09 13:24:08 -0300

    Fixed linters errors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,9 +18,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://vigorous-pike-400a13.netlify.app'
 
-  resource '*',
-            headers: :any,
-            methods: %i[get post put patch delete options head],
-            credentials: true
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
